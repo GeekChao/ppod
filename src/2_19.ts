e779c7b664f164a04b4fa372d2b50f61a01cc204
Kevin Zou <kevinzou19@gmail.com>
2020-08-31 11:15:45 -0400

    ch2

---
+++ src/2_19.ts
@@ -0,0 +1,45 @@
+/**
+ * Summary: Write the changable code
+ *  1. A Class and its method must have a single responsibility. Ask 'w' question or describe its responsibility in a single sentence
+ *  2. Any decision you make in advance of an explicit requirement is just a guess.
+ */
+
+class Gear {
+  public constructor(
+    private chainring: number,
+    private cog: number,
+    private wheel?: Wheel
+  ) {}
+
+  public get ratio() {
+    return this.chainring / this.cog;
+  }
+
+  public get gear_inches() {
+    if (this.wheel) {
+      return this.ratio * this.wheel?.diameter;
+    }
+
+    return undefined;
+  }
+}
+
+class Wheel {
+  public constructor(private rim: number, private tire: number) {}
+
+  public get diameter() {
+    return this.rim + this.tire * 2;
+  }
+
+  public get circumference() {
+    return this.diameter * Math.PI;
+  }
+}
+
+const wheel = new Wheel(26, 1.5);
+console.log(wheel.circumference);
+
+console.log(new Gear(52, 11, wheel).gear_inches);
+
+console.log(new Gear(52, 11).ratio);
+console.log(new Gear(52, 11).gear_inches);
